0ee7632ea2bc53ee5c3d296cda7f27262bbed8da
unknown <nttduonga21162@cusc.ctu.edu.vn>
2022-11-14 19:28:21 +0700

    Push view

--- src/test/Test.java
+++ src/test/Test.java
@@ -3,8 +3,12 @@ package test;
 
 public class Test {
     public static void main(String[] args) {
+<<<<<<< Updated upstream
         System.out.println("Hello");
         System.out.println("Java Version: " + System.getProperty("java.version"));
+=======
+        System.out.println("Version: " + System.getProperty("java.version"));
+>>>>>>> Stashed changes
     }
  
 }
